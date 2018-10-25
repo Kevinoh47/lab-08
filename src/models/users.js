b97ab52b04a4fa8e9d2dc1a7863447dce406ac1e
Kevinoh47 <kohok47@gmail.com>
2018-10-24 17:39:19 -0700

    added tests for notes and users

--- src/models/users.js
+++ src/models/users.js
@@ -61,6 +61,8 @@ class Users {
   }
 }
 
+//helper functions
+
 //regex from https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript#46181
 function validateEmail(email) {
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
