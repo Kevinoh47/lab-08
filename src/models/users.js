cfb9f91c2219d61c30287e4683cb753b73028d7a
Kevinoh47 <kohok47@gmail.com>
2018-10-27 11:59:00 -0700

    fixed linter config and linter errors

--- src/models/users.js
+++ src/models/users.js
@@ -66,7 +66,7 @@ class Users {
 
 //regex from https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript#46181
 function validateEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
 
